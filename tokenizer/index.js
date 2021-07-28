c613c1c6ffec4eefdf21d41350c338607ef767d8
Ellie Wem <ellie.wem@cutover.com>
2021-07-28 21:38:49 +0100

    feat: first operator

--- tokenizer/index.js
+++ tokenizer/index.js
@@ -1,6 +1,17 @@
 class Tokenizer {
   static tokenize(expression = "") {
     if (!expression) return [];
+
+    const split = expression.split(" ");
+    return split.map((literal) => {
+      switch (literal) {
+        case "+":
+          return {
+            literal,
+            type: "operator",
+          };
+      }
+    });
   }
 }
 
--- tokenizer/index.test.js
+++ tokenizer/index.test.js
@@ -8,4 +8,13 @@ describe("Tokenizer", () => {
   it("Returns an empty array when given an empty string", () => {
     expect(Tokenizer.tokenize("")).toEqual([]);
   });
+
+  it("Can tokenize a +", () => {
+    expect(Tokenizer.tokenize("+")).toEqual([
+      {
+        literal: "+",
+        type: "operator",
+      },
+    ]);
+  });
 });
